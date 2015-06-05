b94b0d4bc729db854380d77c98ea68b320f28166
gracewm <gracewm892@gmail.com>
2015-06-05 14:53:01 +0900

    updated

--- plot3.R
+++ plot3.R
@@ -27,7 +27,7 @@ DateTime <- paste(powerData$Date, powerData$Time, sep=" ")
 powerData$DateTime <- as.POSIXct(DateTime)
 
 # Plot multiple line charts and save it as plot3.png.
-png(filename = "plot3.png", width = 480, height = 480, bg ="transparent")
+png(filename = "plot3.png", width = 480, height = 480, bg ="white")
 par(mar = c(6,6,2,2))
 
    with(powerData, {
